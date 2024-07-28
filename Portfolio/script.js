cfa3a064b52e45fde9a798256ced45ec5e41ab9f
Roman-Khadka0 <romankhadkawork@gmail.com>
2024-07-28 14:51:06 +0000

    Portfolio being created

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -2,13 +2,13 @@ let body = document.querySelector('body');
 let buttons = document.querySelectorAll('.button')
 buttons.forEach(function(button){
 button.addEventListener('click',function(e){
-    if(e.target.id==='black'){
-        body.style.backgroundColor = e.target.id;
+    if(e.target.id ==='black'){
+        body.style.backgroundColor = 'black';
         body.style.color = 'white'
     }
-        if(e.target.id==='white'){
-            body.style.backgroundColor =e.target.id ;
-            body.style.color = 'black'
+        if(e.target.id ==='white'){
+            body.style.backgroundColor =  'white' ;
+            body.style.color = 'black';
 
         }
     
